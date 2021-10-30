47226486c9a8998ae6a9d157b90378a888104bc3
Solstice ID <rafifbanner@gmail.com>
2021-10-30 14:14:44 +0700

    struktur ulang project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {ILLogo} from './assets';
+
+const App = () => {
+  return (
+    <View>
+      <ILLogo />
+      <Text>Welcome App JS</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
